b6c19c251f92ce03cfa3858e70ed53a604227af3
Prussia <prussia.hu@gmail.com>
2015-11-30 17:29:01 +0800

    refactoring

--- src/main/java/com/prussia/test/play/spring/dao/DataSourceConfigBean.java
+++ src/main/java/com/prussia/test/play/spring/dao/DataSourceConfigBean.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Description;
 import org.springframework.context.annotation.Profile;
+import org.springframework.context.support.ResourceBundleMessageSource;
 
 @Configuration
 @Profile("dev")
@@ -18,12 +19,14 @@ public class DataSourceConfigBean {
 	private static final Logger log = LoggerFactory.getLogger(DataSourceConfigBean.class);
 
 	DataSource dataSource;
-
+	
+	ResourceBundleMessageSource messageSource;
+	
 	@Bean
 	@ConfigurationProperties(prefix = "spring.datasource")
 	@Description("MySql data source")
 	public DataSource getDataSource() {
 		return DataSourceBuilder.create().build();
 	}
-
+	
 }
