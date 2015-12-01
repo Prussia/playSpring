91d3e8e1a2cd2e7f7b9c103ee10356f2d601a23e
Prussia <prussia.hu@gmail.com>
2015-12-01 14:49:46 +0800

    support thymeleaf

--- src/main/java/com/prussia/test/play/spring/web/GreetingsController.java
+++ src/main/java/com/prussia/test/play/spring/web/GreetingsController.java
@@ -5,13 +5,21 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+/**
+ * Thymeleaf samples
+ * Add the following in pom.xml
+ *  	<dependency>
+			<groupId>org.springframework.boot</groupId>
+			<artifactId>spring-boot-starter-thymeleaf</artifactId>
+		</dependency>
+ *
+ */
 @Controller
 public class GreetingsController {
-	//TODO
 	@RequestMapping("/greetings")
 	public String greetingTp(@RequestParam(value = "name", required = false, defaultValue = "World") String name,
 			Model model) {
 		model.addAttribute("name", name);
-		return "classpath:resources/templates/greetingTp";
+		return "greetingTp";
 	}
 }
