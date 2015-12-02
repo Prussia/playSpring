456c3d8a404a876941d49b6730618eed1ba93f8b
Prussia <prussia.hu@gmail.com>
2015-12-02 17:07:59 +0800

    add comment

--- src/main/java/com/prussia/test/play/spring/repository/CachableCustomerRepository.java
+++ src/main/java/com/prussia/test/play/spring/repository/CachableCustomerRepository.java
@@ -18,4 +18,16 @@ public class CachableCustomerRepository {
 		System.out.println("---> Loading Customer with last name '" + lastName + "'"); //when the value can be found in cache, the method won't be invoked
 		return new Customer(1, "firstName", lastName);
 	}
+	
+	/**
+	 * http://tianmaying.com/tutorial/spring-web-ehcache#15
+	 * 	@Cacheable 触发添加缓存的方法
+		@CacheEvict 触发删除缓存的方法
+		@CachePut 在不干涉方法执行的情况下更新缓存
+		@Caching 组织多个缓存标注的标注
+		@CacheConfig 在class的层次共享缓存的设置
+	 * http://docs.spring.io/spring/docs/current/spring-framework-reference/html/cache.html
+	 * 
+	 * 
+	 */
 }
