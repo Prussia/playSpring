5b91005fa8db7c26ac6f2265c30729df09bedaf5
Prussia <bloosh2005@163.com>
2016-03-30 21:53:00 +0800

    add spring context support

--- src/main/java/com/prussia/test/play/spring/config/CacheConfiguration.java
+++ src/main/java/com/prussia/test/play/spring/config/CacheConfiguration.java
@@ -1,6 +1,8 @@
 package com.prussia.test.play.spring.config;
 
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.ehcache.EhCacheCacheManager;
+import org.springframework.cache.ehcache.EhCacheManagerFactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
