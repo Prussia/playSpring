b71b31dc58b8a48e28c571ef06dfb986cf8b1238
Prussia Hu <prussia.hu@pwc.com>
2016-08-26 16:16:48 +0800

    update log

--- src/main/java/com/prussia/test/play/spring/config/DataSourceConfiguration.java
+++ src/main/java/com/prussia/test/play/spring/config/DataSourceConfiguration.java
@@ -2,8 +2,6 @@ package com.prussia.test.play.spring.config;
 
 import javax.sql.DataSource;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -11,11 +9,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Description;
 import org.springframework.context.annotation.Profile;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Configuration
 @Profile("dev")
+@Slf4j
 public class DataSourceConfiguration {
-
-	private static final Logger log = LoggerFactory.getLogger(DataSourceConfiguration.class);
 
 	DataSource dataSource;
 	
