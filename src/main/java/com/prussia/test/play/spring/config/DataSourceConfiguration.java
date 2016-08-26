ea029dfd83d3d11d3847f100ca5ee4e6a9805af0
Prussia Hu <prussia.hu@pwc.com>
2016-08-26 16:16:09 +0800

    add comments

--- src/main/java/com/prussia/test/play/spring/config/DataSourceConfiguration.java
+++ src/main/java/com/prussia/test/play/spring/config/DataSourceConfiguration.java
@@ -10,7 +10,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Description;
 import org.springframework.context.annotation.Profile;
-import org.springframework.context.support.ResourceBundleMessageSource;
 
 @Configuration
 @Profile("dev")
--- src/main/java/com/prussia/test/play/spring/config/SwaggerConfig.java
+++ src/main/java/com/prussia/test/play/spring/config/SwaggerConfig.java
@@ -3,6 +3,7 @@ package com.prussia.test.play.spring.config;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -14,7 +15,14 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 @EnableAutoConfiguration
+@Profile("dev")
 public class SwaggerConfig {
+	/*
+	 * The json data
+	 * http://localhost:8080/v2/api-docs
+	 * UI access
+	 * http://localhost:8080/swagger-ui.html
+	 */
 	@Bean
 	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2).select()
