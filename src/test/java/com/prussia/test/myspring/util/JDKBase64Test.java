f32d618fd67e98e3486ae77edcbe512f55658a8f
Prussia <prussia.hu@pwc.com>
2017-08-24 16:08:10 +0800

    Update

--- src/test/java/com/prussia/test/myspring/util/JDKBase64Test.java
+++ src/test/java/com/prussia/test/myspring/util/JDKBase64Test.java
@@ -30,4 +30,26 @@ public class JDKBase64Test {
 		String password = new String(bpwd);
 		log.warn("password: {}", password);
 	}
+	
+	@Test
+	public void testBase64Comparation() {
+		String enpassword1 = "V2FzYUBzc2cx";
+
+		byte[] bpwd1 = Base64.getDecoder().decode(enpassword1.getBytes());
+
+		String password1 = new String(bpwd1);
+		log.warn("password: {}", password1);
+		
+		
+		String enpassword2 = "V2FzYUBzc2cx43";
+
+		byte[] bpwd2 = Base64.getDecoder().decode(enpassword2.getBytes());
+
+		String password2 = new String(bpwd2);
+		log.warn("password: {}", password2);
+		
+		Assert.assertNotEquals(password2, password1);
+	}
+	
+	
 }
