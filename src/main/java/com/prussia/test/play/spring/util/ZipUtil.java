e2c9ec46f80776a749553d02baf7b5ff64075f47
Prussia <prussia.hu@gmail.com>
2017-12-05 23:55:47 +0800

    Refactoring

--- src/main/java/com/prussia/test/play/spring/util/ZipUtil.java
+++ src/main/java/com/prussia/test/play/spring/util/ZipUtil.java
@@ -4,18 +4,13 @@ import org.apache.commons.compress.archivers.ArchiveException;
 import org.apache.commons.compress.archivers.ArchiveOutputStream;
 import org.apache.commons.compress.archivers.ArchiveStreamFactory;
 import org.apache.commons.compress.archivers.zip.ZipArchiveEntry;
-import org.apache.commons.compress.archivers.zip.ZipFile;
 import org.apache.commons.compress.utils.IOUtils;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
-import org.junit.Test;
 
 import java.io.*;
 import java.util.Collection;
-import java.util.Enumeration;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.*;
 
 public class ZipUtil {
 
@@ -31,50 +26,51 @@ public class ZipUtil {
 	 * @throws ArchiveException
 	 *             if creating or adding to the archive fails
 	 */
-	private void addFilesToZip(File source, File destination) throws IOException, ArchiveException {
-		OutputStream archiveStream = new FileOutputStream(destination);
-		ArchiveOutputStream archive = new ArchiveStreamFactory().createArchiveOutputStream(ArchiveStreamFactory.ZIP,
-				archiveStream);
+	public static void addFilesToZip(File source, File destination) throws IOException, ArchiveException {
+		try (OutputStream archiveStream = new FileOutputStream(destination);
+				ArchiveOutputStream archive = new ArchiveStreamFactory()
+						.createArchiveOutputStream(ArchiveStreamFactory.ZIP, archiveStream);) {
 
-		if (source.isDirectory()) {
-			Collection<File> fileList = FileUtils.listFiles(source, null, true);
+			if (source.isDirectory()) {
+				Collection<File> fileList = FileUtils.listFiles(source, null, true);
 
-			for (File file : fileList) {
-				String entryName = getEntryName(source, file);
-				zipSingleFile(file, archive, entryName);
+				for (File file : fileList) {
+					String entryName = getEntryName(source, file);
+					zipSingleFile(file, archive, entryName);
+				}
+			} else if (source.isFile()) {
+				String entryName = FilenameUtils.getName(source.getPath());
+				zipSingleFile(source, archive, entryName);
 			}
-		} else if (source.isFile()) {
-			String entryName = FilenameUtils.getName(source.getPath());
-			zipSingleFile(source, archive, entryName);
+
+			archive.finish();
 		}
-
-		archive.finish();
-		archiveStream.close();
 	}
 
-	private void zipSingleFile(File source, ArchiveOutputStream archive, String entryName)
+	private static void zipSingleFile(File source, ArchiveOutputStream archive, String entryName)
 			throws IOException, FileNotFoundException {
 		ZipArchiveEntry entry = new ZipArchiveEntry(entryName);
 		archive.putArchiveEntry(entry);
 
-		BufferedInputStream input = new BufferedInputStream(new FileInputStream(source.getPath()));
+		try(BufferedInputStream input = new BufferedInputStream(new FileInputStream(source.getPath()));){
 
-		IOUtils.copy(input, archive);
-		input.close();
+			IOUtils.copy(input, archive);
+			
+		}
 		archive.closeArchiveEntry();
 	}
 
-	private void zipFile(ArchiveOutputStream archive, File file, String entryName)
-			throws IOException, FileNotFoundException {
-		ZipArchiveEntry entry = new ZipArchiveEntry(entryName);
-		archive.putArchiveEntry(entry);
-
-		BufferedInputStream input = new BufferedInputStream(new FileInputStream(file));
-
-		IOUtils.copy(input, archive);
-		input.close();
-		archive.closeArchiveEntry();
-	}
+//	private void zipFile(ArchiveOutputStream archive, File file, String entryName)
+//			throws IOException, FileNotFoundException {
+//		ZipArchiveEntry entry = new ZipArchiveEntry(entryName);
+//		archive.putArchiveEntry(entry);
+//
+//		BufferedInputStream input = new BufferedInputStream(new FileInputStream(file));
+//
+//		IOUtils.copy(input, archive);
+//		input.close();
+//		archive.closeArchiveEntry();
+//	}
 
 	/**
 	 * Remove the leading part of each entry that contains the source directory name
@@ -87,7 +83,7 @@ public class ZipUtil {
 	 * @throws IOException
 	 *             if the io fails
 	 */
-	private String getEntryName(File source, File file) throws IOException {
+	private static String getEntryName(File source, File file) throws IOException {
 		int index = source.getAbsolutePath().length() + 1;
 		String path = file.getCanonicalPath();
 
