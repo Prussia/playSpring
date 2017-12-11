bc5f6b202f81a6b5a0fad249b18efaefc43f15ea
Prussia <prussia.hu@pwc.com>
2017-12-11 17:44:09 +0800

    kafka stream

--- src/main/java/com/prussia/test/play/spring/config/KafkaConfiguration.java
+++ src/main/java/com/prussia/test/play/spring/config/KafkaConfiguration.java
@@ -2,9 +2,13 @@ package com.prussia.test.play.spring.config;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.apache.kafka.streams.StreamsConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
@@ -20,23 +24,43 @@ public class KafkaConfiguration {
 
 	@Bean
 	public ProducerFactory<String, Map<String, Object>> producerFactory() {
-	    return new DefaultKafkaProducerFactory<>(producerConfigs());
+		return new DefaultKafkaProducerFactory<>(producerConfigs());
 	}
 
 	@Bean
 	public Map<String, Object> producerConfigs() {
-	    Map<String, Object> props = new HashMap<>();
-	    props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-	    props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-	    props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
-	    props.put("max.block.ms", 5000);
-	    // See https://kafka.apache.org/documentation/#producerconfigs for more properties
-	    return props;
+		Map<String, Object> props = new HashMap<>();
+		props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+		props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+		props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+		props.put("max.block.ms", 5000);
+		// See https://kafka.apache.org/documentation/#producerconfigs for more
+		// properties
+		return props;
 	}
 
 	@Bean
 	public KafkaTemplate<String, Map<String, Object>> kafkaTemplate() {
-	    return new KafkaTemplate<String, Map<String, Object>>(producerFactory());
+		return new KafkaTemplate<String, Map<String, Object>>(producerFactory());
+	}
+
+	@Bean
+	public Properties kafkaStreamConfigs() {
+		Properties streamsConfiguration = new Properties();
+		// Give the Streams application a unique name. The name must be unique
+		// in the Kafka cluster
+		// against which the application is run.
+		streamsConfiguration.put(StreamsConfig.APPLICATION_ID_CONFIG, "wordcount-lambda-example");
+		// Where to find Kafka broker(s).
+		streamsConfiguration.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+		// Specify default (de)serializers for record keys and for record
+		// values.
+		streamsConfiguration.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+		streamsConfiguration.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+		
+		streamsConfiguration.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
+
+		return streamsConfiguration;
 	}
 
 }
