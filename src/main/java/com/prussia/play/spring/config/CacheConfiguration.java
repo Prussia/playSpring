b193eaccc025ca1eb087f2b28cb1c2b986c70fa2
Prussia <prussia.hu@gmail.com>
2018-04-04 21:18:51 +0800

    redis config set up

--- src/main/java/com/prussia/play/spring/config/CacheConfiguration.java
+++ src/main/java/com/prussia/play/spring/config/CacheConfiguration.java
@@ -5,34 +5,57 @@ import org.springframework.cache.ehcache.EhCacheCacheManager;
 import org.springframework.cache.ehcache.EhCacheManagerFactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import redis.clients.jedis.Protocol;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+
 /**
- *  http://docs.spring.io/spring/docs/current/spring-framework-reference/html/cache.html
- *  spring-boot-sample-cache
-	https://github.com/spring-projects/spring-boot/tree/master/spring-boot-samples/spring-boot-sample-cache
- *	@Cacheable triggers cache population
- *	@CacheEvict triggers cache eviction
- *	@CachePut updates the cache without interfering with the method execution
- *	@Caching regroups multiple cache operations to be applied on a method
- *	@CacheConfig shares some common cache-related settings at class-level
+ * http://docs.spring.io/spring/docs/current/spring-framework-reference/html/cache.html
+ * spring-boot-sample-cache
+ * https://github.com/spring-projects/spring-boot/tree/master/spring-boot-samples/spring-boot-sample-cache
+ * 
+ * @Cacheable triggers cache population
+ * @CacheEvict triggers cache eviction
+ * @CachePut updates the cache without interfering with the method execution
+ * @Caching regroups multiple cache operations to be applied on a method
+ * @CacheConfig shares some common cache-related settings at class-level
  * @author phu021
  *
  */
 @Configuration
 @EnableCaching // delete it if to be disable caching
-public class CacheConfiguration {
+public class CacheConfiguration extends CachingConfigurerSupport {
 
-	// private final CacheManager cacheManager;
-	//
-	// @Autowired
-	// public CacheConfiguration() {
-	// this.cacheManager = CacheManager.getInstance();
-	// }
-	//
-	// @PreDestroy
-	// public void destroy() {
-	// cacheManager.shutdown();
-	// }
+	@Bean
+	public JedisConnectionFactory redisConnectionFactory() {
+		JedisConnectionFactory redisConnectionFactory = new JedisConnectionFactory();
+		redisConnectionFactory.setHostName("127.0.0.1");
+		redisConnectionFactory.setPort(Protocol.DEFAULT_PORT);
+		return redisConnectionFactory;
+	}
+
+	@Bean
+	public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory cf) {
+		RedisTemplate<String, String> redisTemplate = new RedisTemplate<String, String>();
+		redisTemplate.setConnectionFactory(cf);
+		return redisTemplate;
+	}
+
+	@Bean(name="cacheManager")
+	@Primary
+	public CacheManager cacheManager(RedisTemplate redisTemplate) {
+		RedisCacheManager cacheManager = new RedisCacheManager(redisTemplate);
+		cacheManager.setDefaultExpiration(300);
+		return cacheManager;
+	}
 
 	/*
 	 * ehcache 主要的管理器
